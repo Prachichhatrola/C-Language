eafed793c29b44367304af412092c8dfb027f641
Prachi Chhatrola <prachichhatrola009@gmail.com>
2023-04-08 11:00:30 +0530

    Delete p 2.4.c

--- p 2.4.c
+++
@@ -1,9 +0,0 @@
-int main()
-{
-    float centigrate,fahrenheit;
-    printf("Enter the value of fahrenheit :\n");
-    scanf("%d",&fahrenheit);
-    centigrate=(fahrenheit-32)*5/9;
-    printf("centigrate tanparature is=%.2f",centigrate);
-    return 0;
-}
