3a60d4308dca7862f555dd402e4f59d72ffd42d5
Prachi Chhatrola <prachichhatrola009@gmail.com>
2023-04-08 10:53:28 +0530

    Add files via upload

---
+++ Practical/p 2.2.c
@@ -0,0 +1,14 @@
+int main()
+{
+    int a,b;
+    printf("Enter number a");
+    scanf("%d",&a);
+    printf("Enter number b");
+    scanf("%d",&b);
+    printf("addition of a+b=%d\n",a+b);
+    printf("subtraction of a-b=%d\n",a-b);
+    printf("multiplication of a*b=%d\n",a*b);
+    printf("division of a%b=%d\n",a/b);
+    printf("modulo of a%b=%d\n",a%b);
+    return 0;
+}
