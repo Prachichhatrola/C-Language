a1ef2699e23f7948bde9ab18f8670d6601122a54
Prachi Chhatrola <prachichhatrola009@gmail.com>
2023-04-08 10:35:45 +0530

    Create 1D_Arrays.c

---
+++ HR_Solution/1D_Arrays.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int n,m,sum=0;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&m);
+        sum += m;
+    }
+    printf("%d",sum);
+
+   
+    return 0;
+}
