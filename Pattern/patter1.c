9c03ca9f879b9f7adbae777c7deb8f620904d631
Prachi Chhatrola <prachichhatrola009@gmail.com>
2024-07-20 18:41:33 +0530

    Create patter1.c

---
+++ Pattern/patter1.c
@@ -0,0 +1,27 @@
+// * * *
+// * * *
+// * * *
+  
+public class Solution {
+
+    public static void nForest(int n) {
+
+      for (int i=0;i<n;i++)
+
+      {
+
+          for (int j=0;j<n;j++)
+
+          {
+
+              System.out.print("* ");
+
+          }
+
+          System.out.println();
+
+      }
+
+    }
+
+}
