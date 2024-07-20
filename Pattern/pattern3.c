f58842bbe240189739028d2980df01483de554f4
Prachi Chhatrola <prachichhatrola009@gmail.com>
2024-07-20 18:51:30 +0530

    Create pattern3.c

---
+++ Pattern/pattern3.c
@@ -0,0 +1,15 @@
+// 1
+// 1 2 
+// 1 2 3
+
+public class Solution {
+    public static void nTriangle(int n) {
+        // Write your code here
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
